635696161581db92d54ea4db2c08f88aa1a15a6d
Aatoken <3410760440@qq.com>
2018-06-11 21:12:10 +0800

    实现 recyclerView

--- example/src/main/java/com/atoken/cn/imooc_recyclerview/MainActivity.java
+++ example/src/main/java/com/atoken/cn/imooc_recyclerview/MainActivity.java
@@ -1,13 +1,59 @@
 package com.atoken.cn.imooc_recyclerview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.atoken.cn.imooc_recyclerview.R.id.recyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    RecyclerView mRecyclerView;
+    private List<String> mDatas;
+    private SimpleAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+
+        initData();
+
+
+        //adapter
+        mAdapter = new SimpleAdapter(this, mDatas);
+        mRecyclerView.setAdapter(mAdapter);
+
+        //布局管理
+        LinearLayoutManager manager = new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(manager);
+
+        //设置分割线
+        mRecyclerView.addItemDecoration(BaseDecoration.create(ContextCompat.getColor(this
+                , R.color.app_background), 5));
+
     }
+
+    private void initView() {
+        mRecyclerView = (RecyclerView) findViewById(recyclerView);
+    }
+
+
+    private void initData() {
+        mDatas = new ArrayList<>();
+        for (int i = 'A'; i < 'Z'; i++) {
+            mDatas.add("" + (char) i);
+        }
+    }
+
+
 }
---
+++ example/src/main/java/com/atoken/cn/imooc_recyclerview/SimpleAdapter.java
@@ -0,0 +1,53 @@
+package com.atoken.cn.imooc_recyclerview;
+
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class SimpleAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+    private LayoutInflater mInflater;
+    private Context mContext;
+    private List<String> mDatas;
+
+    public SimpleAdapter(Context mContext, List<String> mDatas) {
+        this.mContext = mContext;
+        this.mDatas = mDatas;
+        this.mInflater=LayoutInflater.from(mContext);
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view=mInflater.inflate(R.layout.item_simple_textview,parent,false);
+        MyViewHolder viewHolder=new MyViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        holder.tv.setText(mDatas.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas.size();
+    }
+}
+
+class MyViewHolder extends RecyclerView.ViewHolder {
+
+    TextView tv;
+    public MyViewHolder(View itemView) {
+        super(itemView);
+
+        tv = (TextView)itemView.findViewById(R.id.id_tv);
+    }
+}
